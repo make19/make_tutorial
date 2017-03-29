58241b9ce97ceb4abf441eb04fb3757dbbec9fa0
make <make@todo.com>
2017-03-29 16:34:33 +0200

    solved problem with hello_word and created publisher

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  //ros::init(argc, argv, "hello_world");
-  //ros::NodeHandle nh;
+  ros::init(argc, argv, "hello_world");
+ ros::NodeHandle nh;
  std::cout << "Hello,world" << std::endl;
 
-//ros::spin();
-  return 0;
+ros::spin();
+
+
+ return 0;
 }
 
 
